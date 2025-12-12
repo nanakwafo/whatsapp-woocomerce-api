83bd5dd3d8040ad1a5f4bbe71d697e350d3c0ab8
nanakwafo <nanamensah1140@gmail.com>
2025-12-12 08:24:33 +0000

    added zip download

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/download-plugin', function () {
+    $path = public_path('whatsapp-shop-ai.zip');
+    return response()->download($path);
+});
