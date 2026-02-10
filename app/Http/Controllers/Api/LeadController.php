dc93f8ed9eb690cf3115915b80f49c7372115b71
nanakwafo <nanamensah1140@gmail.com>
2026-02-10 19:34:29 +0000

    all fixes

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -11,18 +11,22 @@ class LeadController extends Controller
 {
     public function store(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'store_url' => 'nullable|string',
-            'allow_marketing' => 'boolean',
+     
+       $validated = $request->validate([
+            'email'           => 'required|email',
+            'store_url'       => 'nullable|string',
+            'allow_marketing' => 'nullable|boolean',
         ]);
 
+
+         
         Lead::create([
-            'email' => $request->email,
-            'store_url' => $request->store_url,
-            'allow_marketing' => $request->allow_marketing ?? false,
+            'email'           => $validated['email'],
+            'store_url'       => $validated['store_url'] ?? null,
+            'allow_marketing' => $validated['allow_marketing'] ?? false,
         ]);
-
+       
+   
         return response()->json([
             'message' => 'Lead saved.',
             'download_url' => 'https://api.waorders.com/download-plugin'
