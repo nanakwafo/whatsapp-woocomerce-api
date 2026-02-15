9197446932f45c393f61d4e98acf8559562f84d4
nanakwafo <nanamensah1140@gmail.com>
2026-02-15 19:01:58 +0000

    change route

--- routes/api.php
+++ routes/api.php
@@ -23,10 +23,13 @@ Route::middleware(['waorders.signature'])->group(function () {
 
    //send message where session is handle by backend
    Route::post('/message/sendwhatsapp', [MessageController::class, 'send']);
-   //webhook for receiving messages from whatsapp configured on facebook developer portal
-//    Route::post('/webhook/whatsapp', [MessageController::class, 'webhook']);
-   Route::match(['get', 'post'], '/webhook/whatsapp', [MessageController::class, 'webhook']);
+
 }); 
+
+
+   //webhook for receiving messages from whatsapp configured on facebook developer portal
+
+   Route::match(['get', 'post'], '/webhook/whatsapp', [MessageController::class, 'webhook']);
 
 
 
